4b2566da7a94db85402c6d266e6aede884226307
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-04 13:53:22 -0600

    Create database and run migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "foods#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :new, :create, :show, :destroy]
+  resources :publicrecipes, only: [:index]
+  resources :shoppinglist, only: [:index]
 end
