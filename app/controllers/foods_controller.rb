954385d1a9bf5369655aa8d5ebf1748fd383be75
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-05 15:44:52 +0100

    food list setup completed

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,32 @@
 class FoodsController < ApplicationController
-  def index; end
 
-  def new; end
+  def index
+    @food = current_user.foods.includes(:user)
+  end
 
-  def create; end
+  def new
+    @food = Food.new
+  end
 
-  def destroy; end
+  def create
+    @food = current_user.foods.build(food_params)
+    if @food.save
+      redirect_to foods_path, notice: "Food was successfully created."
+    else
+      flash.now[:error] = 'Error occured. Please check your data and try again.'
+      render :new
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: "Food was successfully deleted."
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/foods/index.html.erb',
+    './app/views/foods/new.html.erb',
+    './app/assets/stylesheets/application.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
