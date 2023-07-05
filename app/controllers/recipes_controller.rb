c7299009b8e084981fdc7f3b1d38cdbe16c71b18
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-04 23:47:13 -0600

    Implement creation of new recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,11 +5,33 @@ class RecipesController < ApplicationController
 
   def new
     @user = current_user.name
+    # @recipe = Recipe.find
+    @recipe = Recipes.new
+    respond_to do |format|
+      format.html { render :new, locals: {recipe: @recipe} }
+    end
   end
 
-  def create; end
+  def create
+    @recipe = Recipes.new(recipe_params)
+    @recipe.user = current_user
+    respond_to do |format|
+      format.html do
+        if @recipe.save
+          redirect_to recipes_path
+        else
+          flash.now[:error] = "Error: could not save recipe"
+          render :new, locals: { recipe: @recipe }
+        end
+      end
+    end
+  end
 
   def show; end
 
   def destroy; end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
+  end
 end
