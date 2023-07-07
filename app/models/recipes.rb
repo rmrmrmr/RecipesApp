fce5a295519afe886ca01628fb4652a2ab30588f
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-07 12:31:30 -0600

    Create tests for Recipe model

--- app/models/recipes.rb
+++ app/models/recipes.rb
@@ -1,5 +1,9 @@
 class Recipes < ApplicationRecord
   belongs_to :user
-
-  has_many :recipefoods
+  has_many :recipefoods, dependent: :destroy, foreign_key: 'recipe_id'
+  validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :description, presence: true
+  validates :user_id, presence: true
 end
