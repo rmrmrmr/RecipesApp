d8d3dff80c81b20fc3a590bfb3557574c6de1d46
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-07 12:08:47 -0600

    Create tests for Food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,13 +2,22 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  describe 'validations' do
-    it { should validate_numericality_of(:price).is_greater_than(0) }
-    it { should validate_numericality_of(:quantity).is_greater_than(0) }
+  subject { Food.create(name: 'food', measurement_unit: 'kg', price: 10, quantity: 1, user_id: 1) }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
 
-  describe 'associations' do
-    it { should have_many(:recipe_foods).class_name('RecipeFood').with_foreign_key('food_id') }
-    it { should belong_to(:user).class_name('User') }
+  it 'quantity should not be smaller than 1' do
+    subject.quantity = 0
+    expect(subject).to_not be_valid
+  end
+
+  it 'price should not be smaller than 1' do
+    subject.price = 0
+    expect(subject).to_not be_valid
   end
 end
