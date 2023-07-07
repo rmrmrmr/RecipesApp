7a829fb33ecfa7143fc6fb3a103a15f24d1208f1
rmrmrmr <ricardomartineza77@gmail.com>
2023-07-07 12:46:42 -0600

    Create tests for Recipefood model

--- spec/models/shoppinglist.rb
+++
@@ -1,9 +0,0 @@
-# spec/models/shoppinglist_spec.rb
-require 'rails_helper'
-
-RSpec.describe Shoppinglist, type: :model do
-  describe 'associations' do
-    it { should belong_to(:user) }
-    it { should have_many(:recipefoods) }
-  end
-end
