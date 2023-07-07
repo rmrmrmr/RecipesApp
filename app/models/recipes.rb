4e1c574dd63e6f58f4c16307d41c167581e3e064
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-07 21:31:33 +0100

    shopping list errors worked on

--- app/models/recipes.rb
+++ app/models/recipes.rb
@@ -2,5 +2,5 @@ class Recipes < ApplicationRecord
   belongs_to :user
 
   has_many :recipefoods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
-  has_many :foods, through: :recipefoods, class_name: 'Food'
+  has_many :foods, through: :recipefoods
 end
