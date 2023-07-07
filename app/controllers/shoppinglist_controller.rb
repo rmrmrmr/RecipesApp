27c634da2c1fbd69701e69b6931e7aa27e976ac4
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-07 20:36:28 +0100

    shopping list controller worked on

--- app/controllers/shoppinglist_controller.rb
+++ app/controllers/shoppinglist_controller.rb
@@ -1,11 +1,11 @@
 class ShoppinglistController < ApplicationController
   def general_shopping_list
     @user = current_user.name
-    @recipes = current_user.recipes.includes(:foods)
+    @recipes = Recipe.find(params[:recipe_id])
     @general_food_list = current_user.foods
   
     # Find the missing food items for all recipes
-    @missing_foods = @recipes.map { |recipe| recipe.foods }.flatten.uniq - @general_food_list
+    @missing_foods = @recipes.foods
   
     # Calculate the total count and price of the missing food
     @total_missing_count = @missing_foods.count
